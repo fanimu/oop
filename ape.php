8af83cc3d523a2de9b6a1b865a4a7d903f12ba47
Yepi Septiana <yepi.septiana@smartfren.com>
2020-07-31 17:50:01 +0700

    oop

--- ape.php
+++ ape.php
@@ -5,6 +5,14 @@
 class Ape extends Animal
 {
 	
+	
+	public function __construct($name,$legs=2,$cold_blooded="false"){
+		$this->name = $name;
+		$this->legs = $legs;
+		$this->cold_blooded = $cold_blooded;
+		
+	}
+
 	public function yell(){
 		$suara = "Auoooo";
 
--- frog.php
+++ frog.php
@@ -3,6 +3,13 @@
 
 class Frog extends Animal
 {
+
+	public function __construct($name,$legs=4,$cold_blooded="false"){
+		$this->name = $name;
+		$this->legs = $legs;
+		$this->cold_blooded = $cold_blooded;
+		
+	}
 	
 	public function jump(){
 		$loncat = "Hop hop";
